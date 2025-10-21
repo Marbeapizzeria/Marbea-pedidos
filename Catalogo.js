dd9e7e7f7e51723e8571419e4c81b74711acd4a2
Marbeapizzeria <marbeapizzeria@gmail.com>
2025-10-21 14:58:54 -0300

    Catalogo.js

---
+++ Catalogo.js
@@ -0,0 +1,23 @@
+// catálogojS — Editá libremente este array
+// Tip: mantené nombres únicos para que las notas se vinculen bien.
+window.CATALOGO = [
+  {
+    nombre: "Muzzarella",
+    precio: 7500,
+    img: "img/muzzarella.jpg",
+    notasPH: "Sin orégano, bien dorada…"
+  },
+  {
+    nombre: "Napolitana",
+    precio: 8500,
+    img: "img/napolitana.jpg",
+    notasPH: "Poco ajo, extra tomate…"
+  },
+  {
+    nombre: "Jamón y Morrones",
+    precio: 9500,
+    img: "img/jamon-morrones.jpg",
+    notasPH: "Sin morrón, extra jamón…"
+  }
+  // Agregá más productos duplicando el bloque y cambiando valores
+];
